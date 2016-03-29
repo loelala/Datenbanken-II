c92826d4236573918d68b39e122c273219846eec
Alexandra Vogel <alexandra.vogel@vosoft.de>
2016-03-29 10:00:41 +0200

    test

---
+++ Aufgabe1/club.php
@@ -0,0 +1,12 @@
+<?php 
+
+	if($_POST['formSubmit'] == "Submit") 
+	{
+	   $varClub = $_POST['club'];
+	   $varName = $_POST['formName'];
+	   $varGender = $_POST['formGender'];
+	   $errorMessage = "";
+ 
+ 	}
+
+?>
