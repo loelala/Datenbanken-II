a7eee9ee247397d877353da6ed369ac2a8fe8174
Alexandra Vogel <alexandra.vogel@vosoft.de>
2016-04-04 16:36:37 +0200

    fix 1000000

--- Aufgabe1/db/vereine.php
+++ Aufgabe1/db/vereine.php
@@ -18,9 +18,8 @@ if($con) {
     
     while($row = mysql_fetch_row($query)) {
         for ($j=0; $j < mysql_num_fields($query); $j++) {
-            echo '<option value="'.$row[$j]'">' . $row[$j] .'</option>';
+            echo '<option value="'.$row[$j].'">' . $row[$j] .'</option>';
         }
-        
     }
     
 } else {
