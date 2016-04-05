6acbf7b72632448bf7ac639612edf2ee89c21b61
Alexandra Vogel <alexandra.vogel@vosoft.de>
2016-04-05 09:42:11 +0200

    test

--- Aufgabe1/db/otherVerein.php
+++ Aufgabe1/db/otherVerein.php
@@ -8,13 +8,17 @@
 //     exit; 
 //   }
 
-if ($_POST['club'] == 'other') {
-    echo '<div class="form-group" style="padding-top: 10px;">
-                        <label class="control-label col-sm-4" for="verein">Dein         Lieblingsverein war nicht in der Liste?</label>
-                        <div class="col-sm-8">
-                            <input type="text" name="otherVerein"></input>
-                        </div>
-                    </div>';
-}
+//if ($_POST['club'] == 'other') {
+//    echo '<div class="form-group" style="padding-top: 10px;">
+//                        <label class="control-label col-sm-4" for="verein">Dein         Lieblingsverein war nicht in der Liste?</label>
+//                        <div class="col-sm-8">
+//                            <input type="text" name="otherVerein"></input>
+//                        </div>
+//                    </div>';
+//}
+
+if(isset($_POST['club'])){
+$selected_val = $_POST['club'];  // Storing Selected Value In Variable
+echo "You have selected :" .$selected_val;  // Displaying Selected Value
 
 ?>
