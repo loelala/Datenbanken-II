1a76b81e15e058d14d44a4ab43a79f622d6ad85a
Alexandra Vogel <alexandra.vogel@vosoft.de>
2016-04-11 12:52:43 +0200

    verein anzeigen der ausgewählt wurde

--- Aufgabe1/db/otherVerein.php
+++ Aufgabe1/db/otherVerein.php
@@ -27,7 +27,7 @@ if(isset($_POST['club'])){
     }
     else {
         // Displaying Selected Value
-        echo '<div class="col-sm-12"> <p> Danke dass du für <strong>' .$selected_val. '</strong> gestimmt hast</p></div>';  
+        echo '<div class="well well-lg col-sm-12"> <p> Danke dass du für <strong>' .$selected_val. '</strong> gestimmt hast</p></div>';  
     }
 }
 ?>
